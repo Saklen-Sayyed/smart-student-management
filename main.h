cba05f400cfc528ec79139a80624bca6b5d116bd
Saklen Sayyed <saklensayyedofficial@gmail.com>
2025-11-12 16:20:14 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,17 @@
+
+#define NAME_SIZE 20
+
+void speak(const char *text);
+void append_record(void);
+void insert_record(void);
+void search_record(void);
+void edit_record(void);
+struct student* search(void);
+void delete_all(void);
+void display_all(void);
+void delete_record(void);
+void midpoint(void);
+void reverse_display(struct student *);
+void sort_record(void);
+unsigned short int lastnode(void);
+void save_file(void);
